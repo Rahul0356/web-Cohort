03f38f4b63c5c4a500d1d5bc883db7d6a87c0735
rahul <rahulkumar9199270356@gmail.com>
2025-03-04 10:48:58 +0530

    machine coding

---
+++ Js-class-week-03/fun.js
@@ -0,0 +1,12 @@
+Object.prototype.chai = function(){
+    console.log("chai aur code");
+
+}
+const tea = {
+    name:"Ice tea lemon",
+    type:"cool",
+}
+tea.chai()
+
+const myTeas = ["lemon tea","orange tea"];
+myTeas.chai()
---
+++ Js-class-week-03/learn.md
@@ -0,0 +1,1 @@
+#Learn about the Quokka extensions
