1775a0e71b6364ea45d87e8d84b3f0fb550832ab
rahul <rahulkumar9199270356@gmail.com>
2025-03-07 12:43:54 +0530

    if_else_basic

---
+++ Speedjs-week-03/ifelse03.js
@@ -0,0 +1,13 @@
+function prepareChai(type){
+
+
+if(type === "Masala Chai"){
+    console.log("Adding spices to the chai");
+} else{
+    
+    console.log("Preparing regular chai")
+}
+}
+prepareChai("Masala Chai");
+prepareChai("Masala Chai"); 
+
