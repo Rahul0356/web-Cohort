da6bb4160dac1695d86ab39d1bbff4d14c8eacd5
rahul <rahulkumar9199270356@gmail.com>
2025-03-10 15:10:00 +0530

    basic promise example

--- Speedjs-week-03/08_promise.js
+++ Speedjs-week-03/08_promise.js
@@ -1,4 +1,6 @@
-console.log("Hi Rahul");
-setTimeout(()=> console.log('Hello after 2 s'),2*1000);
-Promise.resolve().then(()=>console.log('Promise Resolve hogya'));
-console.log('Bye')
+console.log("Hello Rahul Arya how are you")
+
+setTimeout(()=>console.log('Hello after 2 s'),2 *1000);
+Promise.resolve().then(()=>console.log('Promise Ressolve hogya'));
+
+console.log('Bye');
