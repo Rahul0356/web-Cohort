8f5c60e78a7f67fe035a979f2bb87d37e0ef27ed
rahul <rahulkumar9199270356@gmail.com>
2025-03-11 14:41:03 +0530

    proxy-understanding

---
+++ Speedjs-week-03/09_Hosting.js
@@ -0,0 +1,13 @@
+console.log('age is ',age);
+
+//DEAD ZONE 
+let age =25;
+
+//RefernceError:age is not defined
+//Cannot access 'age' before initialization
+
+
+//do not excute BCZ of TDZ(temporal dead zone)
+
+
+
