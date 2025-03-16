87b782f31b98f4169ef38a12fa842f93812e2b71
rahul <rahulkumar9199270356@gmail.com>
2025-03-16 11:44:28 +0530

    init

---
+++ JS-class-week-04/promiseBasic.js
@@ -0,0 +1,11 @@
+console.log("Hello"); 
+let a = 10;
+let b = 20;
+
+function getWeatherInfo() {
+    return 30; 
+}
+
+let temp = getWeatherInfo();
+let sum = a + b + temp;
+console.log("Sum:", sum);
