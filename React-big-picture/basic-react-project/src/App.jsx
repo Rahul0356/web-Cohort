cc3b3bae6c9e0d89e9d51924f5e693336cbc985d
rahul <rahulkumar9199270356@gmail.com>
2025-05-06 23:27:32 +0530

    Basic- React

---
+++ React-big-picture/basic-react-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import  Login from './components/login.jsx'
+const App = () => {
+  return (
+    <>
+    <Login/>
+      <div>App</div>
+     
+    </>
+  )
+}
+
+export default App
---
+++ React-big-picture/basic-react-project/src/components/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>Main founda to know to basic setUp to react-project</div>
+  )
+}
+
+export default login
